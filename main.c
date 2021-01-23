56ae4e0869f3b374b10b487119ef120d30adc84f
alelekime <alessandrass11@yahoo.com>
2021-01-23 04:46:59 -0300

    funcoes.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "funcoes.h"
+
+int main(int argc, char const *argv[]) {
+  int socket_confirmado;
+
+  linha_comando entrada;
+  system ("clear");
+
+
+  lista_comandos_ajuda();
+  if (argc == 1) {
+    printf("nada\n");
+      //lista_comandos_ajuda();
+  }else if (!strcmp( argv[1], "cliente")) {
+    socket_confirmado = ConexaoRawSocket();
+    printf("MODO CLIENTE ..\n");
+    while(1){
+      imprime_path();
+      le_comando(&entrada);
+      printf(" main %s\n", entrada.comando);
+      printf("main %s\n", entrada.diretorio);
+
+}
+
+  }else if (!strcmp( argv[1], "servidor")){
+    socket_confirmado = ConexaoRawSocket();
+    printf("MODO SERVIDOR ..\n");
+
+  }
+
+}
