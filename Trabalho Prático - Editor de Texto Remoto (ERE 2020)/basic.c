9e1dbef18121c544e16d447861a038430ea88111
Alessandra Souza <42846638+alelekime@users.noreply.github.com>
2022-04-05 18:02:48 +0000

    Add files via upload

---
+++ Trabalho Prático - Editor de Texto Remoto (ERE 2020)/basic.c
@@ -0,0 +1,221 @@
+#include "funcoes.h"
+
+long decimalToBinary(int decimalnum){
+    long binarynum = 0;
+    int rem, temp = 1;
+    while (decimalnum!=0)
+    {
+        rem = decimalnum%2;
+        decimalnum = decimalnum / 2;
+        binarynum = binarynum + rem*temp;
+        temp = temp * 10;
+    }
+    return binarynum;
+}
+int convert(long long n) {
+    int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
+
+int verifica(estrutura_pacote *p) {
+  int p_origem, p_destino;//origem == chegando do servidor; destino == pariedade atual
+  p_origem = p-> pariedade;
+  p_destino = cal_pariedade(p-> tamanho, p-> sequencia, p->tipo, p->dados, p-> endereco_origem, p->endereco_destino);
+
+  printf("%d, %d\n",p_origem,p_destino );
+
+  return 0;
+}
+
+int stringToDecimal(char * str){
+    int result =0;
+int len=strlen(str);
+for(int i=0,j=len-1;i<len;i++,j--)
+    {
+    result += ((int)str[i] - 48)*pow(10,j);
+    }
+
+   return result;
+}
+
+
+int acha_binario(int tam) {
+  switch (tam) {
+    case 1:
+    tam = 1;
+    break;
+    case 2:
+    tam = 10;
+    break;
+    case 3:
+    tam = 11;
+    break;
+    case 4:
+    tam = 100;
+    break;
+    case 5:
+    tam = 101;
+    break;
+    case 6:
+    tam = 110;
+    break;
+    case 7:
+    tam = 111;
+    break;
+    case 8:
+    tam = 1000;
+    break;
+    case 9:
+    tam = 1001;
+    break;
+    case 10:
+    tam = 1010;
+    break;
+    case 11:
+    tam = 1011;
+    break;
+    case 12:
+    tam = 1100;
+    break;
+    case 13:
+    tam = 1101;
+    break;
+    case 14:
+    tam = 1110;
+    break;
+    case 15:
+    tam = 1111;
+    break;
+
+  }
+  return tam;
+}
+
+
+
+int cal_pariedade(int tam, int seq, int tipo, char *dado, char *endereco_origem, char*endereco_destino) {
+  int par = 0;
+  int binary;
+  int soma = tam + seq + tipo;
+  while (soma > 0) {
+      par = par+ soma %10;
+      soma = soma / 10;
+  }
+  int tamanho =  strlen(dado);
+  for(int i = 0; i < 2; i++){
+      par = par ^ *endereco_origem;
+      *endereco_origem++;
+  }
+  for(int i = 0; i < 2; i++){
+      par = par ^ *endereco_destino;
+      *endereco_destino++;
+  }
+  for(int i = 0; i < tamanho; i++){
+      par = par ^ *dado;
+      *dado++;
+  }
+  binary = decimalToBinary(par);
+
+
+  return binary;
+}
+
+void imprime_path() {
+  char* diretorio_atual;
+  char endereco[PATH_MAX +1];
+  diretorio_atual = getcwd(endereco, PATH_MAX + 1 );
+  printf( "%s$", diretorio_atual );
+}
+
+char * convert_binary(int dec, char *output) {
+    if (dec ==11 ) {
+      strcpy(output, "0011");
+    } else if (dec == 111) {
+      strcpy(output, "0111");
+    }else if (dec == 1111) {
+      strcpy(output, "1111");
+    }else if (dec == 10) {
+      strcpy(output, "0010");
+    }else if (dec == 110) {
+      strcpy(output, "0110");
+    }else if (dec == 1110) {
+      strcpy(output, "1110");
+    }else if (dec == 1011) {
+      strcpy(output, "1011");
+    }else if (dec == 1010) {
+      strcpy(output, "1010");
+    }else {
+      output[3] = (dec & 1) + '0';
+      output[2] = ((dec >> 1) & 1) + '0';
+      output[1] = ((dec >> 2) & 1) + '0';
+      output[0] = ((dec >> 3) & 1) + '0';
+    }
+    return output;
+}
+
+char * int2bin(unsigned i, int tam){
+    size_t bits = tam;
+
+    char * str = malloc(bits + 1);
+    if(!str) return NULL;
+    str[bits] = 0;
+
+    // type punning because signed shift is implementation-defined
+    unsigned u = *(unsigned *)&i;
+    for(; bits--; u >>= 1){
+        str[bits] = u & 1 ? '1' : '0';
+        //printf(".%c\n", str[bits]);
+}
+    return str;
+}
+
+
+void lista_comandos_ajuda() {
+  printf("-----------------------------------------------------------\n");
+  printf(" * cd <nome_dir>\n");
+  printf(" * lcd <nome_dir> \n");
+  printf(" * ls \n");
+  printf(" * lls \n");
+  printf(" * ver <nome_arq> \n");
+  printf(" * ver <nome_arq> \n");
+  printf(" * linha <numero_linha> <nome_arq>\n");
+  printf(" * linhas <numero_linha_inicial> <numero_linha_final> <nome_arq> \n");
+  printf(" * edit <numero_linha> <nome_arq> “<NOVO_TEXTO>”  \n");
+  printf("-----------------------------------------------------------\n");
+}
+
+void mostra_protocolo(estrutura_pacote *p1) {
+  printf(" MARCADOR %s\n", p1 -> marcador);
+  printf(" TAMANHO %d\n", p1 -> tamanho);
+  printf(" ENDEREÇO DE ORIGEM %s\n", p1 -> endereco_origem);
+  printf(" ENDEREÇO DE DESTINO %s\n", p1 -> endereco_destino);
+  printf(" SEQUÊNCIA %d\n", p1 -> sequencia);
+  switch (p1 -> tipo) {
+    case 0:
+    printf(" TIPO 0000\n");
+    break;
+    case 1:
+    printf(" TIPO 0001\n");
+    break;
+    case 10:
+    printf(" TIPO 0010\n");
+    break;
+    case 11:
+    printf(" TIPO 0011\n");
+    break;
+    case 100:
+    printf(" TIPO 0100\n");
+    break;
+    case 101:
+    printf(" TIPO 0101\n");
+    break;
+  }
+  printf(" DADO %s", p1 -> dados);
+  printf(" PARIEDADE %d\n", p1 -> pariedade );
+}
---
+++ Trabalho Prático - Editor de Texto Remoto (ERE 2020)/funcoes.h
@@ -0,0 +1,96 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/ethernet.h>
+#include <linux/if_packet.h>
+#include <linux/if.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <poll.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+
+#define ENTRADA_MAX 4096
+#define MARCA_INICIO 01111110
+#define PATH_MAX 4096
+#define CD 0000
+#define LS 0001
+#define VER 10
+#define LINHA 0011
+#define EDIT 0101
+#define ACK 1000
+#define NACK 1001
+#define CONTEUDO_LS 1011
+#define ERR 1111
+
+
+typedef struct linha_comando {
+  char *comando;
+  char *diretorio;
+  char *nome_arq;
+  char *dados;
+  int linha;
+  int linha_inicial;
+  int linha_final;
+}linha_comando;
+
+typedef struct estrutura_pacote {
+  char *marcador;
+  int tamanho;
+  char *endereco_origem;
+  char *endereco_destino;
+  int sequencia;
+  int tipo;
+  char *dados;
+  int pariedade;
+}estrutura_pacote;
+
+estrutura_pacote *protocolo(char *dado,int tipo, int tam, int seq);
+estrutura_pacote* abre_protocolo(char *entrada_server);
+char* recebe_protocolo(int socket) ;
+void lista_comandos_ajuda();
+int ConexaoRawSocket();
+void client_LLS();
+void imprime_path();
+void le_comando( linha_comando *entrada, int socket);
+int cal_pariedade(int tam, int seq, int tipo, char *dado, char *endereco_origem, char*endereco_destino);
+long decimalToBinary(int decimalnum);
+void mostra_protocolo(estrutura_pacote *pacote);
+void client_CD(linha_comando *entrada, int socket) ;
+char *protocolo_string(estrutura_pacote * p1) ;
+void client_LS(linha_comando *entrada, int socket) ;
+char * int2bin(unsigned i, int tam);
+int convert(long long n);
+int envia_protocolo(char *string, int socket) ;
+void funcoes_server(int socket_confirmado);
+void server_CD(estrutura_pacote *p, int socket);
+int server_LS(estrutura_pacote *p, int socket) ;
+int server_LINHA(estrutura_pacote *p, int socket);
+int server_LINHAS(estrutura_pacote *p, int socket);
+int server_EDIT(estrutura_pacote *p, int socket);
+int client_LINHAS(linha_comando *entrada, int socket);
+int client_EDIT(linha_comando *entrada, int socket);
+estrutura_pacote *protocolo_server(char *dado,int tipo, int tam, int seq);
+void server_VER(estrutura_pacote *p, int socket);
+void client_VER(linha_comando *entrada, int socket);
+bool recebe_ls(estrutura_pacote * p1, int socket);
+int LINHAS(FILE *nome, int linha, int socket);
+int split_string(char *string,int cont, int tam, int socket);
+//char *protocolo_string_server(estrutura_pacote * p1);//
+int strlcpy(char *dst, const char *src, size_t siz);
+int acha_binario(int tam) ;
+char * convert_binary(int dec, char *output);
+int client_LINHA(linha_comando *entrada, int socket) ;
+int stringToDecimal(char * str);
+int verifica(estrutura_pacote *p);
+estrutura_pacote *protocolo_client(char *dado, int tipo, int tam, int seq);
